acbbfe4ae8e8632b6d4ddb1fd6427af94d5a5ccd
Numpkens <numpkins188@gmail.com>
2025-05-25 15:48:13 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 let firstCard = 10;
 let secondCard = 4;
-
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-
 let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+// 2. Store the sum paragraph in a variable called sumEl
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card");
-}
-else if (sum === 21) {
-    console.log("Hell ya! You have Blackjack!");
-}
-else {
-    console.log("Oh! No!");
-    isAlive = false;
-};
+    // 3. Render the sum on the page using this format -> "Sum: 14"
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
 
-console.log(isAlive);
